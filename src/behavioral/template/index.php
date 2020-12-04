c4d6d3d429b7131732314836463baa56a9eda2f8
Akim_MI <akim@doubledmarketing.com>
2020-12-04 18:51:39 +0200

    switch to match php 8

--- src/behavioral/template/index.php
+++ src/behavioral/template/index.php
@@ -20,14 +20,11 @@ echo "\nChoose the social network to post the message:\n" .
     "2 - Twitter\n";
 $choice = readline();
 
-// Now, let's create a proper social network object and send the message.
-if ((int)$choice === 1) {
-    $network = new Facebook($username, $password);
-} elseif ((int)$choice === 2) {
-    $network = new Twitter($username, $password);
-} else {
-    die("Sorry, I'm not sure what you mean by that.\n");
-}
+match ((int)$choice) {
+    1       => $network = new Facebook($username, $password),
+    2       => $network = new Twitter($username, $password),
+    default => die("Sorry, I'm not sure what you mean by that.\n")
+};
 
 $network->post($message);
 
