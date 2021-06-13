36e286ca45c60e187e883849de2dced8a20e690a
Akim_MI <akim@doubledmarketing.com>
2021-06-13 14:30:12 +0300

    builder

---
+++ src/creational/builder/PostgresQueryBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Builder;
+
+/**
+ * Этот Конкретный Строитель совместим с PostgreSQL. Хотя Postgres очень похож
+ * на Mysql, в нем всё же есть ряд отличий. Чтобы повторно использовать общий
+ * код, мы расширяем его от строителя MySQL, переопределяя некоторые шаги
+ * построения.
+ */
+
+class PostgresQueryBuilder extends MysqlQueryBuilder
+{
+    /**
+     * Помимо прочего, PostgreSQL имеет несколько иной синтаксис LIMIT.
+     * @throws \Exception
+     */
+    public function limit(int $start, int $offset): SQLQueryBuilder
+    {
+        parent::limit($start, $offset);
+
+        $this->query->limit = " LIMIT " . $start . " OFFSET " . $offset;
+
+        return $this;
+    }
+
+    // + тонны других переопределений...
+
+}
--- src/junk/junk.php
+++ src/junk/junk.php
@@ -1,59 +1,9 @@
 <?php
-
-abstract class AbstractTool
-{
-    /**
-     * @param int $playerNumber
-     * @return string
-     */
-    public function announceWinner(int $playerNumber): string
-    {
-        return 'The winner is player number: ' . $playerNumber;
-    }
-}
-
-class Rock extends AbstractTool
-{
-    public string $weakerEntity   = Scissors::class;
-    public string $strongerEntity = Paper::class;
-}
-
-
-class Paper extends AbstractTool
-{
-    public string $weakerEntity   = Rock::class;
-    public string $strongerEntity = Scissors::class;
-}
-
-
-class Scissors extends AbstractTool
-{
-    public string $weakerEntity   = Paper::class;
-    public string $strongerEntity = Rock::class;
-}
-
-class RockPaperScissor
-{
-    public function rockPaperScissors(AbstractTool $tool1, AbstractTool $tool2): string
-    {
-        if ($tool1 instanceof $tool2) {
-            return 'Nobody wins';
-        }
-
-        if ($tool1->strongerEntity === $tool2->weakerEntity) {
-            return $tool1->announceWinner(1);
-        }
-
-        if ($tool1->weakerEntity === $tool2->strongerEntity) {
-            return $tool2->announceWinner(2);
-        }
-
-        throw new RuntimeException('Oops');
-    }
-}
-
-
-$rockPaperScissors = new RockPaperScissor();
-
-echo $rockPaperScissors->rockPaperScissors(new Scissors(), new Paper());
-
+/**
+ * In this case, we want to increase the default cost for BCRYPT to 12.
+ * Note that we also switched to BCRYPT, which will always be 60 characters.
+ */
+$options = [
+    'cost' => 12,
+];
+echo password_hash("rasmuslerdorf", PASSWORD_BCRYPT, $options);
